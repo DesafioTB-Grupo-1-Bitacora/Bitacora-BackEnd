410eb8d5280dd4b0cee5fe0e3e9e0356ffd5f76c
Fulgencio <funche_em@hotmail.com>
2023-08-07 16:02:53 +0200

    strong password validation

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,6 @@
 const slonik = require("slonik");
 
-// const db = slonik.createPool(process.env.DB_URL);
+//const db = slonik.createPool(process.env.DB_URL);
 const db = {};
 
 module.exports = db;
--- src/middlewares/fieldsValidator.js
+++ src/middlewares/fieldsValidator.js
@@ -1,20 +1,24 @@
-const errors = require('../misc/errors')
+const errors = require("../misc/errors");
 
-module.exports = (...fields) => (req, _, next) => {
-    for(let field of fields) {
-        if(!req.body[field]) return next(errors[400])
+module.exports =
+  (...fields) =>
+  (req, _, next) => {
+    for (let field of fields) {
+      if (!req.body[field]) return next(errors[400]);
     }
 
-    const { password } = req.body
-    
-    if(password && password.length < 12) return next(errors["pass_length"])
+    const { password } = req.body;
+    // Validaciones de contraseña robusta
+    if (password && password.length < 12) return next(errors["pass_length"]);
 
-    /* Aquí habría que incluir el resto de validaciones
-        *
-        *
-        * 
-        * 
-     */
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasNumbers = /[0-9]/.test(password);
+    const hasSymbols = /[!@#$%^&*()_+[\]{};':"\\|,.<>/?]/.test(password);
 
-    next()
-}
+    if (!(hasLowerCase && hasUpperCase && hasNumbers && hasSymbols)) {
+      return next(errors["pass_complexity"]);
+    }
+
+    next();
+  };
