4dd52015359a9e45fdba341df9387f44c8dc9658
Fulgencio <funche_em@hotmail.com>
2023-08-08 20:17:45 +0200

    Set recommended HTTP headers

--- src/main.js
+++ src/main.js
@@ -5,21 +5,36 @@ const errors = require("./misc/errors");
 const routes = require("./routes");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Strict-Transport-Security",
+    "max-age=63072000; includeSubDomains"
+  );
+  res.setHeader("Content-Security-Policy", "default-src 'self'");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  next();
+});
+
 app.use(express.json());
-app.use('/bitacora-service', routes(db));
+app.use("/bitacora-service", routes(db));
 
 app.use((_, __, next) => {
-    next(errors[404])
-})
+  next(errors[404]);
+});
 
 app.use(({ statusCode, error }, _, res, __) => {
-    res.status(statusCode).json({
-        success: false,
-        message: error.message,
-    })
-})
+  res.status(statusCode).json({
+    success: false,
+    message: error.message,
+  });
+});
 
-app.listen(process.env.PORT, () => console.log("> Listening at:", process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log("> Listening at:", process.env.PORT)
+);
 
-console.log('DB', {name: process.env.DB_NAME})
+console.log("DB", { name: process.env.DB_NAME });
 module.exports = app;
