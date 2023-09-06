d0d21167d5748ef12326f9408bcb8b1f191a3194
Sergio-Cano <sergio95cano@hotmail.com>
2023-09-06 10:18:20 +0200

    Added temporally hardcoded constants

--- src/misc/authUtils.js
+++ src/misc/authUtils.js
@@ -12,8 +12,10 @@ const comparePass = (plain) => async (hash) => {
     return await bcrypt.compare(plain, hash);
 };
 
+const SECRET = "dnqPNyveEefq5nkihDS71cWXhPA25VywV6rzfGUN5yUOtU5pYYlXcqCyfZS4iMsqJdMIwgLTRjJbTzVxnZm9Mqf3TR16woXhmC4aVbmwzbzgQYcRrxjIVX68WxJEer0G"
+
 const serializeToken = (res, payload) => {
-    const token = jwt.sign(payload, process.env.SECRET);
+    const token = jwt.sign(payload, SECRET);
 
     res.cookie("access_token", token, {
       expires: new Date(Date.now() + (60 * 60 * 1000)),
--- src/misc/cloudinary.js
+++ src/misc/cloudinary.js
@@ -2,12 +2,21 @@ require("dotenv").config({ path: "../../.env" });
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
+//Hardcodeado temporalmente
+
 cloudinary.config({
+    cloud_name: "ddetpj28z",
+    api_key: "649934916723966",
+    api_secret: "gYlN2-cbwUYNDIG1FnMsM1oTG1U",
+    secure: true,
+})
+
+/* cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
-})
+}) */
 
 const checkFolder = async (folder) => {
     try {
