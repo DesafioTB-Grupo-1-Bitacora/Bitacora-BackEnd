40c5cfa6702449dfafd0c9775863616ce2526b49
kikeiz <kikeizp96@gmail.com>
2023-09-06 17:53:12 +0200

    connection to postgres fixed

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
+process.env.PGSSLMODE = 'no-verify';
+
 const db = new Pool({
     user: 'postgres',
     password: 'postgres1234',
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    port: Number(process.env.DB_PORT)
 })
 
 module.exports = db;
