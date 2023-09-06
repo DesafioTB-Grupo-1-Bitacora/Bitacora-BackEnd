b76edee3760b5c8e38ef9dcc4ae8142d23769182
kikeiz <kikeizp96@gmail.com>
2023-09-06 23:17:18 +0200

    Set header in response

--- src/main.js
+++ src/main.js
@@ -26,26 +26,8 @@ app.use(({ statusCode, error }, _, res, __) => {
     success: false,
     message: error.message,
   });
-});
 
-app.use((err, req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );     
-
-  //Check sintax error in request
-  if (err instanceof SyntaxError) {
-    Logger.error(err);
-    return ResponseGenerator.error(res,Status.REQUEST_ERROR,"JSON sended was bad formatted",Codes.Reporting.REQUEST_ERROR);
-  }
-  next();
+  res.header('Access-Control-Allow-Origin', '*');  
 });
 
 app.listen(process.env.PORT, () =>
